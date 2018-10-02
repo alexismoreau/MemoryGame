3f8dc3412f33f52f4dd6773810c0ffb5b3e5381b
Alexis Moreau <moreau.alexis@morman.fr>
2018-10-02 22:39:30 +0200

    import figures

---
+++ starter-kit/src/scripts/utils/figures.js
@@ -0,0 +1,16 @@
+const figures = [
+  'man',
+  'bear',
+  'snake',
+  'horse',
+  'rat',
+  'bee',
+  'beef',
+  'donkey',
+  'camel',
+  'spider',
+  'pork',
+  'rabbit',
+];
+
+export default figures;
